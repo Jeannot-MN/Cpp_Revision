48e3c4301cd9da91b0be709df07c115522d18c13
JeannotMN <jeannot@ssquared.dev>
2022-08-16 20:24:30 +0200

    favourite number

---
+++ favourite_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number;
+
+    cout << "Enter your favourite number: ";
+    cin >> number;
+
+    cout<<"Great! "<< number << " is my favourite number too!" << endl;
+
+    system("pause");
+    return 0;
+}
