44f5e4601a85f0722b84a3fd88064562dae7a63e
JeannotMN <jeannot@ssquared.dev>
2022-08-29 18:05:40 +0200

    scanf and pointers

---
+++ scanf-with-array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char username [6];
+    puts("Enter the username: ");
+    scanf("%s", &username);
+
+    printf("The username is: %s", username);
+    return 0;
+}
