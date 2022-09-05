92e6a905aacd81c34926d336532c8269815c853b
JeannotMN <jeannot@ssquared.dev>
2022-09-05 22:29:10 +0200

    implementaion of the linked list

--- doubly-linked-list/dll.c
+++ doubly-linked-list/dll.c
@@ -3,3 +3,29 @@
 
 #include "dll.h"
 
+
+doubly_linked_list *get_new_dll(){
+    doubly_linked_list *dll = calloc(1, sizeof(doubly_linked_list));
+    dll->head = NULL;
+    dll->tail = NULL;
+
+    return dll;
+}
+
+int add_data_to_dll(doubly_linked_list *dll, int *data){
+    if(!dll || !data) return -1;
+
+    node *new_node = calloc(1, sizeof(node));
+
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->previous  = NULL;
+
+    if(dll->head == NULL){
+        dll->head = new_node;
+        return 0;
+    }
+
+    dll->tail->next = new_node;
+    dll->tail = new_node;
+}
--- doubly-linked-list/dll.h
+++ doubly-linked-list/dll.h
@@ -1,13 +1,14 @@
 /* Header file for Doubly Linked List */
 
 typedef struct node {
-    void *data;
+    int *data;
     struct node *previous;
     struct node *next;
 } node;
 
 typedef struct doubly_linked_list {
     node *head;
+    node *tail;
 } doubly_linked_list;
 
 /* Function to create and return
@@ -18,4 +19,4 @@ doubly_linked_list *get_new_dll();
 /*  Funtion to insert a node at the end of
  *  the given doubly linked list
  */
-int add_data_to_dll(doubly_linked_list *dll, void *data);
+int add_data_to_dll(doubly_linked_list *dll, int *data);
