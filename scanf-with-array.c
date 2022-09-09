252ce44501d2a800b3dd1ad49314fbdcf48eb074
JeannotMN <jeannot@ssquared.dev>
2022-09-09 21:40:30 +0200

    refactoring

--- scanf-with-array.c
+++ scanf-with-array.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
-    char username[6];
+int main(){
+    char username [6];
     puts("Enter the username: ");
     scanf("%s", &username);
 
     printf("The username is: %s", username);
 
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
+    for(int i =0; i< 5; i++){
+        for(int j=0; j<=i; j++){
             printf("*");
         }
         printf("\n");
