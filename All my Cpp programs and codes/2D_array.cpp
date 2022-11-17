427e1922686c904621bcfee7ccf8eb734264c831
JeannotMN <jeannot@ssquared.dev>
2022-11-17 21:27:02 +0200

    My C++ days projects code

--- All my Cpp programs and codes/2D_array.cpp
+++ All my Cpp programs and codes/2D_array.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-	int my_array[4][4] = { {9,5,6,2}, {1,2,3,4}, {3,6,9,0}, {3,7,2,1}};
-	                      
-	for (int i =0; i<4; i++)
+	int my_array[4][4] = {{9, 5, 6, 2}, {1, 2, 3, 4}, {3, 6, 9, 0}, {3, 7, 2, 1}};
+
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j=0; j<4; j++)
+		for (int j = 0; j < 4; j++)
 		{
-			cout<< my_array[i][j]<<"  ";
+			cout << my_array[i][j] << "  ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
 	system("PAUSE");
-	return 0;       
+	return 0;
 }
