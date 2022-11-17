a65e6f8c07474cd7e5fb3f05ea305465a67b3ddb
JeannotMN <jeannot@ssquared.dev>
2022-11-17 21:27:51 +0200

    My C++ days projects code

--- All my Cpp programs and codes/ternary.cpp
+++ All my Cpp programs and codes/ternary.cpp
@@ -2,11 +2,12 @@
 #include <string>
 using namespace std;
 
-int main(){
-	
-	int a = (2>3)? 2 : 3;
-	cout<< a<<endl;
-	
+int main()
+{
+
+	int a = (2 > 3) ? 2 : 3;
+	cout << a << endl;
+
 	system("pause");
 	return 0;
 }
